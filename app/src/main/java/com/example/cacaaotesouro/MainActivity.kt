5d75718d1b82ddd9d4d12759c9004bdc276de3c3
pauloFrancisconi <pauloffrancisconi@gmail.com>
2025-03-31 16:26:56 -0300

    Adicionando tela final e navegação final

--- app/src/main/java/com/example/cacaaotesouro/MainActivity.kt
+++ app/src/main/java/com/example/cacaaotesouro/MainActivity.kt
@@ -14,7 +14,12 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.compose.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,9 +48,21 @@ fun AppNavigation() {
         }
         composable("tela03") {
             Tela03Screen(
-                onNavigateToTela02 = { navigationController.navigate("tela02") },
-                onNavigateToHome = { navigationController.navigate("home") }
+                onNavigateToHome = { navigationController.navigate("home") },
+                onNavigateToTela04 = { navigationController.navigate("tela04") }
             )
+        }
+
+        composable("tela04"){
+            Tela04Screen(
+                onNavigateToHome = { navigationController.navigate("home") },
+                onNavigateToTelaParabens = { navigationController.navigate("tela05") }
+
+            )
+        }
+
+        composable("tela05") {
+            Tela05Screen(navController = navigationController)
         }
     }
 }
@@ -70,12 +87,10 @@ fun HomeScreen(onNavigateToTela02: () -> Unit,) {
 
 @Composable
 
-fun Tela02Screen(onNavigateToHome: () -> Unit, onNavigateToTela03: () -> Unit) {
+fun Tela02Screen(onNavigateToHome: () -> Unit, onNavigateToTela03: () -> Unit,) {
     var respostaQSt1 = remember { mutableStateOf(TextFieldValue()) }
 
-    Button(
-        onClick = {onNavigateToHome()}
-    ) { Text(text = "Voltar") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -108,20 +123,116 @@ fun Tela02Screen(onNavigateToHome: () -> Unit, onNavigateToTela03: () -> Unit) {
 
 
 @Composable
-fun Tela03Screen(onNavigateToTela02: () -> Unit, onNavigateToHome: () -> Unit) {
+fun Tela03Screen(onNavigateToTela04: () -> Unit, onNavigateToHome: () -> Unit) {
+
+
+    var respostaQSt2 = remember { mutableStateOf(TextFieldValue()) }
+
+    Button(
+        modifier = Modifier.padding(20.dp),
+        onClick = onNavigateToHome
+    ) { Text(text = "Voltar") }
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("Desafio 2:", fontSize = 24.sp)
-        Button(onClick = onNavigateToTela02) { Text("Ir para Tela 02") }
-        Button(onClick = onNavigateToHome) { Text("Voltar para Home") }
+        Text("Desafio 2: ", fontSize = 24.sp)
+        Text(text = "O que é leve como uma pena, mas nem a pessoa mais forte consegue segurar por muito tempo?", fontSize = 16.sp)
+
+
+        TextField(
+            value = respostaQSt2.value,
+            onValueChange = { newText -> respostaQSt2.value = newText },
+            label = { Text("Resposta") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                if (respostaQSt2.value.text.lowercase() == "respiracao") {
+                    onNavigateToTela04()
+                } else {
+                    println("errou")
+                }
+            }
+        ) {
+            Text("Responder")
+        }
     }
 }
+
+
+@Composable
+fun Tela04Screen(onNavigateToTelaParabens: () -> Unit, onNavigateToHome: () -> Unit) {
+    var respostaQSt3 = remember { mutableStateOf(TextFieldValue()) }
+
+    Button(
+        modifier = Modifier.padding(20.dp),
+        onClick = onNavigateToHome
+    ) { Text(text = "Voltar") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Desafio 3: ", fontSize = 24.sp)
+        Text(text = "O que é que sobe, mas nunca desce?", fontSize = 16.sp)
+
+
+        TextField(
+            value = respostaQSt3.value,
+            onValueChange = { newText -> respostaQSt3.value = newText },
+            label = { Text("Resposta") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                if (respostaQSt3.value.text.lowercase() == "idade") {
+                    onNavigateToTelaParabens()
+                } else {
+                    println("errou")
+                }
+            }
+        ) {
+            Text("Responder")
+        }
+    }
+}
+
+@Composable
+fun Tela05Screen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Parabéns, Você encontrou o tesouro!", fontSize = 24.sp)
+
+
+        Image(
+            painter = painterResource(id = R.drawable.parabens),
+            contentDescription = "Imagem de Parabéns",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(onClick = {
+            navController.navigate("home") {
+                popUpTo("home") { inclusive = true }
+            }
+        }) {
+            Text("Voltar a home")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun Teste() {
-    Tela03Screen(onNavigateToTela02 = {}, onNavigateToHome = {})
+    Tela03Screen(onNavigateToTela04 = {}, onNavigateToHome = {})
 }
